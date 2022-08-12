7f99c8726058753df64d9c825f18719abfa0833c
Mark <mark.vogel1@icloud.com>
2022-08-12 17:29:02 -0600

    Fix ActionBarUtil sending message

--- Images-v1_19_R1/src/main/java/com/andavin/images/v1_19_R1/ActionBarUtil.java
+++ Images-v1_19_R1/src/main/java/com/andavin/images/v1_19_R1/ActionBarUtil.java
@@ -24,7 +24,7 @@
 package com.andavin.images.v1_19_R1;
 
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.protocol.game.ClientboundSystemChatPacket;
+import net.minecraft.network.protocol.game.ClientboundSetActionBarTextPacket;
 import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
@@ -37,6 +37,6 @@ class ActionBarUtil extends com.andavin.util.ActionBarUtil {
     @Override
     protected void sendMessage(Player player, String message) {
         ((CraftPlayer) player).getHandle().connection.send(
-                new ClientboundSystemChatPacket(Component.literal(message), 2));
+                new ClientboundSetActionBarTextPacket(Component.literal(message)));
     }
 }
